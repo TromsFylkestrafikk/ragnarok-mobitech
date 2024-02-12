8b7f6d560d19cb2951845544b6bc2d3f67e6aa59
tft-kyrre <kyrre.andr.hansen@tffk.no>
2024-02-12 14:29:53 +0100

    Issue #1: Fetching data from multiple folders.

--- src/Services/MobitechFiles.php
+++ src/Services/MobitechFiles.php
@@ -10,6 +10,27 @@ class MobitechFiles
 {
     use LogPrintf;
 
+    /**
+     * External folders to download data from.
+     *
+     * @var array
+     */
+    protected $folders = [
+        // Norled
+        '101010/ferry-statistics/%s',
+        '101010/orca-transactions/%s',
+        '101010/statistics/legs/%s',
+
+        // Torghatten Nord
+        '101677/ferry-statistics/%s',
+        '101677/statistics/legs/%s',
+
+        // Boreal
+        '101678/ferry-statistics/%s',
+        '101678/orca-transactions/%s',
+        '101678/statistics/legs/%s',
+    ];
+
     public function __construct()
     {
         $this->logPrintfInit('[MobitechService]: ');
@@ -25,25 +46,34 @@ class MobitechFiles
     public function getData(string $id)
     {
         $this->debug('Fetching Mobitech data with id %s...', $id);
-
-        // Get external file list.
-        $url = sprintf(config('ragnarok_mobitech.file_list_url'), $id);
-        $result = Http::withToken(MobitechAuth::getApiToken())->get($url)->json();
-        if (!isset($result['paths'])) {
-            $this->warning('No transactions available for %s', $id);
-            return [];
-        }
-
-        // Download all files.
         $data = [];
-        $fileCount = 0;
-        foreach ($result['paths'] as $fileInfo) {
-            $url = sprintf(config('ragnarok_mobitech.download_url'), $fileInfo['name']);
-            $content = Http::withToken(MobitechAuth::getApiToken())->get($url)->body();
-            $data[basename($fileInfo['name'])] = $content;
-            $fileCount += 1;
+        $total = 0;
+        foreach ($this->folders as $folder) {
+            // Get external file list.
+            $dir = sprintf($folder, $id);
+            $url = sprintf(config('ragnarok_mobitech.file_list_url'), $dir);
+            $result = Http::withToken(MobitechAuth::getApiToken())->get($url)->json();
+            if (!isset($result['paths'])) {
+                $this->warning('No files found in external folder %s', $dir);
+                continue;
+            }
+
+            // Download all files.
+            $fileCount = 0;
+            foreach ($result['paths'] as $fileInfo) {
+                $url = sprintf(config('ragnarok_mobitech.download_url'), $fileInfo['name']);
+                $content = Http::withToken(MobitechAuth::getApiToken())->get($url)->body();
+
+                // Constructing a human readable filename for the content.
+                $pathName = str_replace('/', '_', $folder);
+                $filename = sprintf($pathName, basename($fileInfo['name']));
+                $data[$filename] = $content;
+                $fileCount += 1;
+            }
+            $total += $fileCount;
+            $this->debug('Downloaded %d file(s) from %s', $fileCount, $dir);
         }
-        $this->debug('Downloaded %d file(s).', $fileCount);
+        $this->debug('Total: %d file(s)', $total);
         return $data;
     }
 }
