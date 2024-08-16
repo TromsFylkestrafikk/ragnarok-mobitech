e365cc28a8506dc1ccf6d174c4e0a1dc9f6ff073
tft-kyrre <kyrre.andr.hansen@tffk.no>
2024-08-16 15:16:33 +0200

    Integer (id) and char columns expanded.

---
+++ database/migrations/2024_08_16_104600_mobitech_expand_columns.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mobitech_transactions', function (Blueprint $table) {
+            $table->bigInteger('line_id')->comment('Skyttel (sales place) line ID')->change();
+            $table->bigInteger('actor_id')->comment('Skyttel (sales place) actor/operator ID')->change();
+            $table->bigInteger('tour_id')->comment('Skyttel (transaction trip) tour ID')->change();
+            $table->bigInteger('stop_place_id_entry')->comment('Skyttel stop place ID (start)')->change();
+            $table->bigInteger('stop_place_id_exit')->comment('Skyttel stop place ID (exit)')->change();
+            $table->char('tariff_class')->change();
+        });
+
+        Schema::table('mobitech_statistics', function (Blueprint $table) {
+            $table->bigInteger('actor_id')->comment('Skyttel (sales place) actor/operator ID')->change();
+            $table->bigInteger('line_id')->comment('Skyttel (sales place) line ID')->change();
+            $table->bigInteger('tour_id')->comment('Skyttel (transaction trip) tour ID')->change();
+            $table->bigInteger('stop_place_id_entry')->comment('Skyttel stop place ID (start)')->change();
+            $table->bigInteger('stop_place_id_exit')->comment('Skyttel stop place ID (exit)')->change();
+        });
+
+        Schema::table('mobitech_actors', function (Blueprint $table) {
+            $table->bigInteger('id')->comment('Unique actor ID')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mobitech_transactions', function (Blueprint $table) {
+            $table->integer('line_id')->comment('Skyttel (sales place) line ID')->change();
+            $table->integer('actor_id')->comment('Skyttel (sales place) actor/operator ID')->change();
+            $table->integer('tour_id')->comment('Skyttel (transaction trip) tour ID')->change();
+            $table->integer('stop_place_id_entry')->comment('Skyttel stop place ID (start)')->change();
+            $table->integer('stop_place_id_exit')->comment('Skyttel stop place ID (exit)')->change();
+            $table->char('tariff_class', 16)->change();
+        });
+
+        Schema::table('mobitech_statistics', function (Blueprint $table) {
+            $table->integer('actor_id')->comment('Skyttel (sales place) actor/operator ID')->change();
+            $table->integer('line_id')->comment('Skyttel (sales place) line ID')->change();
+            $table->integer('tour_id')->comment('Skyttel (transaction trip) tour ID')->change();
+            $table->integer('stop_place_id_entry')->comment('Skyttel stop place ID (start)')->change();
+            $table->integer('stop_place_id_exit')->comment('Skyttel stop place ID (exit)')->change();
+        });
+
+        Schema::table('mobitech_actors', function (Blueprint $table) {
+            $table->integer('id')->comment('Unique actor ID')->change();
+        });
+    }
+};
